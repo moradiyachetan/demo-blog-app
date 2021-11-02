05c46809d5235c9b918ebc892c0a6c211e8b6404
CHETAN MORADIYA <cheten@cupidknot.com>
2021-11-02 10:04:54 +0530

    Set Limit Login Attempts

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,8 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
-
+    protected $maxAttempts = 3; // default is 5
+    protected $decayMinutes = 2; // default is 1
     /**
      * Create a new controller instance.
      *
